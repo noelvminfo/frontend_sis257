f206b82fd1c65fc2a905d7597614565c7f86320f
enevaca <noel.vm.2501@gmail.com>
2023-06-03 12:57:39 -0400

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,86 @@
+<script setup lang="ts">
+import { useRoute } from "vue-router";
+const location = useRoute();
+import { useAuthStore } from "@/stores/index";
+const authStore = useAuthStore();
+</script>
+<template>
+  <header class="site-header">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12 col-12 d-flex flex-wrap">
+          <p class="d-flex me-4 mb-0">
+            <i class="bi-person custom-icon me-2"></i>
+            <strong class="text-dark"
+              >Bienvenido al repositorio Music de SIS257</strong
+            >
+          </p>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <nav
+    class="navbar navbar-expand-lg"
+    :style="location.path != '/' ? 'background-color: black !important' : ''"
+  >
+    <div class="container">
+      <RouterLink to="/" class="navbar-brand"> Music SIS257 </RouterLink>
+
+      <RouterLink v-if="!authStore.token" to="/login" class="btn custom-btn d-lg-none ms-auto me-4">
+        Iniciar Sesión
+      </RouterLink>
+
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav align-items-lg-center ms-auto me-lg-5">
+          <li class="nav-item">
+            <RouterLink to="/" class="nav-link click-scroll">Inicio</RouterLink>
+          </li>
+
+          <li class="nav-item">
+            <RouterLink to="/about" class="nav-link click-scroll"
+              >Acerca De</RouterLink
+            >
+          </li>
+
+          <li class="nav-item">
+            <RouterLink v-if="authStore.token" to="/interpretes" class="nav-link click-scroll"
+              >Intérpretes</RouterLink
+            >
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link click-scroll" href="#section_4">Albumes</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link click-scroll" href="#section_5">Canciones</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link click-scroll" href="#section_6">Contacto</a>
+          </li>
+        </ul>
+
+        <RouterLink v-if="!authStore.token" to="/login" class="btn custom-btn d-lg-block d-none">
+          Iniciar Sesión
+        </RouterLink>
+        <a v-else @click="authStore.logout()" class="btn custom-btn d-lg-block d-none">
+          Cerrar Sesión
+        </a>
+      </div>
+    </div>
+  </nav>
+</template>
